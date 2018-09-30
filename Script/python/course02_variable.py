6cb74a279c001f4d082d7562b8c9d1a8d8192fb3
Kenec <nnamani.kenechukwu@gmail.com>
2018-09-30 18:27:39 +0100

    python script variables

---
+++ Script/python/course02_variable.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import sys
+# Variables can only contain numbers, string or underscores
+
+hello = 'Hello'
+name = 'Kenechukwu'
+role = 'DevOps'
+age = int(sys.argv[1])
+
+print hello, name, '. You\'re and awesome', role
+print 'You are', age, 'old'
